6330fca613deb0b85b9bcb64750c8e6709b2b629
itsamy003 <ammi.uel@gmail.com>
2019-04-17 21:05:32 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 }
+
+@Component({
+  selector: 'fence-comp',
+  template: `<div class="fence">
+      <ng-content></ng-content>
+   </div>`,
+  styles: [`.fence{width:200px; height: 200px; border: 1px solid red;}`]
+})
+export class FenceComponent  {
+ 
+}
+let instance = 0;
+@Component({
+  selector: 'sheep-comp',
+  template: `<div class="sheep">🐑 {{count}}</div>`,
+  styles: [`.sheep{color: green;}`]
+})
+export class SheepComponent  {
+ public count = ++instance;
+}
